e4d52153a55d7b3c46a8b0ceece8be9299f6f1b1
Nidhi ponnalwar <nidhiponnalwar13@gmail.com>
2026-08-30 16:03:51 +0530

    feat: enhance MealItem and MealItemForm components with cart functionality and validation

--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -1,9 +1,22 @@
+import { useContext } from 'react';
 import  './MealItem.css';
 import MealItemForm from './MealItemForm';
+import CartContext from '../../../store/cart-context';
+
 
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
+
   const price = `$${props.price.toFixed(2)}`;
 
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
   return (
     <li className="meal">
       <div>
@@ -12,7 +25,7 @@ const MealItem = (props) => {
         <div className="price">{price}</div>
       </div>
       <div>
-        <MealItemForm/>
+        <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
