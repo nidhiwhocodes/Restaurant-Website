bb4e1eb2990946e57745139b0bef902a04db1201
Nidhi ponnalwar <nidhiponnalwar13@gmail.com>
2026-08-30 15:38:38 +0530

    feat: implement Cart context and provider for managing cart state

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,14 +1,21 @@
+import {useContext} from 'react';
+import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import  './HeaderCartButton.css';
 
 const HeaderCartButton = (props) => {
+   const cartCtx= useContext(CartContext);
+
+   const numberOfCartItems= cartCtx.items.reduce((curNumber, item) => {
+      return curNumber + item.amount;
+   }, 0);
   return (
     <button className="button" onClick={props.onClick}>
       <span className="icon">
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className="badge">3</span>
+      <span className="badge">{numberOfCartItems}</span>
     </button>
   );
 };
