cf3d0d8c4fac486b9a8bcc807c4ca639447ef907
Nidhi ponnalwar <nidhiponnalwar13@gmail.com>
2026-08-30 16:24:46 +0530

    feat: implement CartItem component and integrate with Cart for dynamic item rendering

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,31 @@
-import './Cart.css';
+import { useContext } from 'react';
+
 import Modal from '../UI/Modal';
+import CartItem from './CartItem';
+import  './Cart.css';
+import CartContext from '../../store/cart-context';
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = () => {};
+
+  const cartItemAddHandler = () => {};
+
   const cartItems = (
     <ul className="cart-items">
-      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
@@ -15,13 +35,13 @@ const Cart = (props) => {
       {cartItems}
       <div className="total">
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className="actions">
         <button className="button--alt" onClick={props.onClose}>
           Close
         </button>
-        <button className="button">Order</button>
+        {hasItems && <button className="button">Order</button>}
       </div>
     </Modal>
   );
