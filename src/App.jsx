87671a8c1cb07d7d48c6e0247bccf45c292769ad
Nidhi ponnalwar <nidhiponnalwar13@gmail.com>
2026-08-30 14:56:58 +0530

    feat: implement Cart component with styles and modal functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import  { Fragment } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 import "./App.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <Fragment>
+      <Cart/>
       <Header />
 
       <Meals />
